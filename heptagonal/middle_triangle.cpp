52478ccdff027ec4f688b1ec0e502db14ced04f4
Isaac van der Meulen <isaac.vandermeulen@gmail.com>
2023-01-07 20:22:48 -0800

    Implement all possible splits of heptagonal::MiddleTriangle

--- heptagonal/middle_triangle.cpp
+++ heptagonal/middle_triangle.cpp
@@ -10,13 +10,30 @@ MiddleTriangle::MiddleTriangle(const Point& a, const Point& b, const Point& c)
     : Triangle(a, b, c)
 {}
 
+static std::vector<std::unique_ptr<Triangle>> split_1(const Point& a, const Point& b, const Point& c)
+{
+    const auto d = c*(1.0/(beta*alpha))+a*(1.0/alpha);
+    std::vector<std::unique_ptr<Triangle>> result;
+    result.push_back(std::make_unique<TallTriangle>(b, a, d));
+    result.push_back(std::make_unique<ScaleneTriangle>(b, c, d));
+    return result;
+}
+
+static std::vector<std::unique_ptr<Triangle>> split_2(const Point& a, const Point& b, const Point& c)
+{
+    const auto d = c*(1.0/(beta*beta*alpha))+b*(1.0/(beta*beta));
+    std::vector<std::unique_ptr<Triangle>> result;
+    result.push_back(std::make_unique<MiddleTriangle>(d, a, c));
+    result.push_back(std::make_unique<ScaleneTriangle>(a, b, d));
+    return result;
+}
+
 std::vector<std::unique_ptr<Triangle>> MiddleTriangle::split(const float scale) const
 {
-    const auto d = m_b*(1.0/(beta*alpha))+m_a*(1.0/alpha);
-    std::vector<std::unique_ptr<Triangle>> result;
-    result.push_back(std::make_unique<TallTriangle>(m_c*scale, m_a*scale, d*scale));
-    result.push_back(std::make_unique<ScaleneTriangle>(m_c*scale, m_b*scale, d*scale));
-    return result;
+    return split_1(m_a, m_b, m_c);
+    //return split_1(m_a, m_c, m_b);
+    //return split_2(m_a, m_b, m_c);
+    //return split_2(m_a, m_c, m_b);
 }
 
 std::string MiddleTriangle::label() const
