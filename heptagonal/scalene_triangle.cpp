9ec5b32a68937b17c4094ed2b9bb2186723a8fe2
Isaac van der Meulen <isaac.vandermeulen@gmail.com>
2023-01-07 20:42:47 -0800

    Implement all possible splits of heptagonal::ScaleneTriangle

--- heptagonal/scalene_triangle.cpp
+++ heptagonal/scalene_triangle.cpp
@@ -2,6 +2,7 @@
 
 #include "constants.hpp"
 #include "middle_triangle.hpp"
+#include "tall_triangle.hpp"
 #include "wide_triangle.hpp"
 
 namespace heptagonal {
@@ -10,13 +11,48 @@ ScaleneTriangle::ScaleneTriangle(const Point& a, const Point& b, const Point& c)
     : Triangle(a, b, c)
 {}
 
+static std::vector<std::unique_ptr<Triangle>> split_1(const Point& a, const Point& b, const Point& c)
+{
+    const auto d = b*(1/(alpha*beta))+a*(1/alpha);
+    std::vector<std::unique_ptr<Triangle>> result;
+    result.push_back(std::make_unique<WideTriangle>(d, a, c));
+    result.push_back(std::make_unique<MiddleTriangle>(c, d, b));
+    return result;
+}
+
+static std::vector<std::unique_ptr<Triangle>> split_2(const Point& a, const Point& b, const Point& c)
+{
+    const auto d = a*(1/(alpha*alpha*beta*beta))+b*(1/beta);
+    std::vector<std::unique_ptr<Triangle>> result;
+    result.push_back(std::make_unique<ScaleneTriangle>(c, b, d));
+    result.push_back(std::make_unique<TallTriangle>(a, d, c));
+    return result;
+}
+
+static std::vector<std::unique_ptr<Triangle>> split_3(const Point& a, const Point& b, const Point& c)
+{
+    const auto d = a*(1/(alpha*beta*beta))+b*(1/(beta*beta));
+    std::vector<std::unique_ptr<Triangle>> result;
+    result.push_back(std::make_unique<ScaleneTriangle>(a, c, d));
+    result.push_back(std::make_unique<MiddleTriangle>(d, b, c));
+    return result;
+}
+
+static std::vector<std::unique_ptr<Triangle>> split_4(const Point& a, const Point& b, const Point& c)
+{
+    const auto d = a*(1/(alpha*alpha))+c*(beta/alpha);
+    std::vector<std::unique_ptr<Triangle>> result;
+    result.push_back(std::make_unique<ScaleneTriangle>(b, d, c));
+    result.push_back(std::make_unique<WideTriangle>(d, a, b));
+    return result;
+}
+
 std::vector<std::unique_ptr<Triangle>> ScaleneTriangle::split(const float scale) const
 {
-    const auto d = m_b*(1/(alpha*beta))+m_a*(1/alpha);
-    std::vector<std::unique_ptr<Triangle>> result;
-    result.push_back(std::make_unique<WideTriangle>(d*scale, m_a*scale, m_c*scale));
-    result.push_back(std::make_unique<MiddleTriangle>(m_c*scale, d*scale, m_b*scale));
-    return result;
+    //return split_1(m_a, m_b, m_c);
+    //return split_2(m_a, m_b, m_c);
+    //return split_3(m_a, m_b, m_c);
+    return split_4(m_a, m_b, m_c);
 }
 
 std::string ScaleneTriangle::label() const
